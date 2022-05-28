ef58c8fdb03bfed3da3361da324ece7e8341cded
Taira Davey <tairadavey@Tairas-MacBook-Pro.local>
2022-05-28 14:29:38 -0700

    split date and time for easier copying

--- timeConversion.js
+++ timeConversion.js
@@ -1,13 +1,25 @@
 const dateStart = document.getElementById('date-start');
+const timeStart = document.getElementById('time-start');
+
 const dateEnd = document.getElementById('date-end');
+const timeEnd = document.getElementById('time-end');
 
 // Date range from SFX will look something like `05/25/2022 03:25:36 pm to 05/25/2022 03:40:36 pm`
 // Convert to UTC for splunk searches
-function convertDateRange(dateRange) {    
-    convertedDateRange = dateRange.split(" to ")
-        .map(dateString => new Date(dateString))
-        .map(date => date.toLocaleString('en-US', { timeZone: 'UTC', hourCycle: "h24" }))
+function convertDateRange(datetimeRange) {    
+    convertedDatetimeRange = datetimeRange.split(" to ")
+        .map(datetime => new Date(datetime))
+        .map(datetime => {
+            return [
+                datetime.toLocaleString('en-US', { timeZone: 'UTC', dateStyle: "short"}),
+                datetime.toLocaleString('en-US', { timeZone: 'UTC', hourCycle: "h24", timeStyle: "medium" }) 
+                    + "." + ('00' + datetime.getMilliseconds()).slice(-3)
+            ]
+        })
 
-    dateStart.textContent = convertedDateRange[0];
-    dateEnd.textContent = convertedDateRange[1];
+    dateStart.textContent = convertedDatetimeRange[0][0];
+    timeStart.textContent = convertedDatetimeRange[0][1];
+
+    dateEnd.textContent = convertedDatetimeRange[1][0];
+    timeEnd.textContent = convertedDatetimeRange[1][1];
 }
